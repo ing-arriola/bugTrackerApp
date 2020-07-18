602fd48e88b411bbffd7d5519bf28308c9ee8b09
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-18 15:26:27 -0600

    now the app is able to store new users on mongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcrypt");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -17,12 +18,36 @@ router.post(
       "Please enter a password with 6 or more chareaters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "Email already exists" });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(12);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User Addded");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
