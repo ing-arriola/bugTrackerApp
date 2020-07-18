0d19b96574c501fe9c4e0bc91cfeded0f7f8e045
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-18 13:35:14 -0600

    Defined task endpoind & solved some details with other endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-//@route    GET api/users
+//@route    GET api/auth
 //@desc     Get logged a user
 //@access   Private
 router.get("/", (req, res) => {
   res.send("Get Logged in user");
 });
 
-//@route    Post api/users
+//@route    Post api/auth
 //@desc     Auth user & get token
-//@access   Public
+//@access   Private
 router.post("/", (req, res) => {
-  res.send("Log in user");
+  res.send("Add contact");
 });
 
 module.exports = router;
---
+++ routes/tasks.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+//@route    GET api/task
+//@desc     Get all contacts
+//@access   Private
+router.get("/", (req, res) => {
+  res.send("Get all task");
+});
+
+//@route    POST api/task
+//@desc     Add new contact
+//@access   Private
+router.post("/", (req, res) => {
+  res.send("Add a new task");
+});
+
+//@route    PUT api/users/:id
+//@desc     update contact
+//@access   Private
+router.put("/", (req, res) => {
+  res.send("update task");
+});
+
+//@route    Post api/users
+//@desc     Delete contact
+//@access   Private
+router.delete("/", (req, res) => {
+  res.send("delete task");
+});
+
+module.exports = router;
