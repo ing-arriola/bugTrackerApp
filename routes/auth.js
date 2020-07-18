5a3fc4fe08d78112e302ae8c9dcb690d2fe1326d
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-18 17:26:00 -0600

    completed protected route api/auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const auth = require("../middleware/auth");
+const aut = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 //@route    GET api/auth
 //@desc     Get logged a user
 //@access   Private
-router.get("/", auth, (req, res) => {
-  res.send("Get Logged in user");
+router.get("/", aut, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error on get");
+  }
 });
 
 //@route    Post api/auth
