31e747295ac55fb7e1b182b10d3dae82523c788c
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-19 14:43:22 -0600

    Added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Navbar from "./components/layouts/Navbar";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Jaime Arriola App</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      Jaime Arriola App
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 const Navbar = ({ title, icon }) => {
-  return <div></div>;
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Task Tracker",
+  icon: "fas fa-list",
 };
 
 export default Navbar;
