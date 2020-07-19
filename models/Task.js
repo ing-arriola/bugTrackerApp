dfd768e3a4bf101cf738379e33971f6b04e61c57
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-18 22:57:48 -0600

    task schema created

---
+++ models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("task", TaskSchema);
