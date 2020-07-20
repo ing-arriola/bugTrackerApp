914efd7c35608c4cf1ae9d37ee4b4faed76844e4
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-19 18:13:46 -0600

    taskState ready for a test and already added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-
+import TaskState from "./context/task/TaskState";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <TaskState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </TaskState>
   );
 };
 
--- client/src/context/task/TaskState.js
+++ client/src/context/task/TaskState.js
@@ -2,3 +2,54 @@ import React, { useReducer } from "react";
 import uuid from "uuid";
 import TaskContext from "./taskContext";
 import taskReducer from "./taskReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const TaskState = (props) => {
+  const initialState = {
+    tasks: [
+      {
+        id: 1,
+        name: "create endpoint to insert users",
+        description:
+          "I must create and test the enpoint related with user suscription",
+        status: "Not started yet",
+      },
+      {
+        id: 2,
+        name: "create route to insert taks",
+        description:
+          "I must create and test the route related with add new tasks",
+        status: "just created but not tested",
+      },
+      {
+        id: 3,
+        name: "View for user suscription",
+        description:
+          "I must create and test the view where user make a suscription",
+        status: "Pending to create components but already made the desing",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(taskReducer, initialState);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks: state.tasks,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+};
+
+export default TaskState;
