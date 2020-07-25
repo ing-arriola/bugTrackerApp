b25ff76bf632c41ee6fa2d98c4e5267ec4d39c4b
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-25 17:29:05 -0600

    user is now able to login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
 
-const Login = () => {
+const Login = (props) => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+
+    if (error === "Invalid credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -11,7 +32,14 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Loggin submitted");
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      login({
+        email,
+        password,
+      });
+    }
   };
 
   return (
@@ -19,7 +47,7 @@ const Login = () => {
       <h1>
         User <span className="text-primary">Login</span>
       </h1>
-      <form onSubmit="onSubmit">
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
           <input
@@ -27,6 +55,7 @@ const Login = () => {
             name="email"
             value={email}
             onChange={onChange}
+            required
           ></input>
         </div>
         <div className="form-group">
@@ -36,6 +65,7 @@ const Login = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
           ></input>
         </div>
         <input
