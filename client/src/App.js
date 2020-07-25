430cba1283c8df64fc1bab84bd7cabcd9fd9991a
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-25 09:47:06 -0600

    AllertState added

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from "./components/auth/Login";
 
 import TaskState from "./context/task/TaskState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 import "./App.css";
 
@@ -15,19 +16,21 @@ const App = () => {
   return (
     <AuthState>
       <TaskState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </TaskState>
     </AuthState>
   );
